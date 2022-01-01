7c32373018bb6442af518920509d3c0394f60eb0
sajalkhan <sajalhossen@yahoo.com>
2022-01-02 00:09:44 +0600

    implement form validation

--- chapter-2/script.js
+++ chapter-2/script.js
@@ -1,4 +1,21 @@
 const form = document.querySelector(".signup-form");
+const message = document.querySelector(".message");
+const userName = document.querySelector('input[name="username"]');
+
+//*Validation
+userName.addEventListener("keyup", (e) => {
+  const valid = /^[a-zA-Z 0-9]{6,12}$/;
+  const name = e.target.value;
+
+  if (valid.test(name) || !name) {
+    message.style.display = "none";
+    userName.removeAttribute("style");
+  } else {
+    message.style.display = "block";
+    message.textContent = "Invalid user name";
+    userName.setAttribute("style", "border: 1px solid red");
+  }
+});
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();

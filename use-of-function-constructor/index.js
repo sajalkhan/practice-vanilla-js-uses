2aeea8d5298726f0697b0a45531af5b560454810
sajalkhan <sajalhossen@yahoo.com>
2022-10-29 13:32:05 +0600

    include inheritance example

--- use-of-function-constructor/index.js
+++ use-of-function-constructor/index.js
@@ -1,29 +1,39 @@
-const Person = function (name, email, phone, age) {
+//* ফাংশন কন্সট্রাকটর  ক্লাসের মতো অবজেক্ট এর ব্লু-প্রিন্ট তৈরী করতে ইউজ করা হয়।
+
+const Person = function (name, email, phone) {
   this.name = name;
   this.email = email;
   this.phone = phone;
-  this.getFullInfo = function () {
-    return `${this.name} born in ${2022 - age}`;
-  };
+};
+
+//! 👇 inheritance example
+const Teacher = function (name, email, phone, subject) {
+  Person.call(this, name, email, phone); //! Use inheritance
+  this.subject = subject;
 };
 
 const firstUserElement = document.querySelector(".user1");
 const secondUserElement = document.querySelector(".user2");
+const thirdUserElement = document.querySelector(".user3");
 const button = document.querySelector(".button");
+
+const appendChild = (element, item, info) => {
+  element.innerText = info;
+  !item.children.length && item.appendChild(element);
+};
 
 const showUserDetailsInformation = (element, user) => {
   Array.from(element.children).forEach((item) => {
     const span = document.createElement("span");
 
     if (item.textContent.includes("Name")) {
-      span.innerText = user.name;
-      !item.children.length && item.appendChild(span);
+      appendChild(span, item, user.name);
     } else if (item.textContent.includes("Email")) {
-      span.innerText = user.email;
-      !item.children.length && item.appendChild(span);
+      appendChild(span, item, user.email);
+    } else if (item.textContent.includes("Phone")) {
+      appendChild(span, item, user.phone);
     } else {
-      span.innerText = user.phone;
-      !item.children.length && item.appendChild(span);
+      appendChild(span, item, user.subject);
     }
   });
 };
@@ -31,8 +41,11 @@ const showUserDetailsInformation = (element, user) => {
 const user1 = new Person("sajal", "sajal@gmail.com", 1234); //! function constructor
 showUserDetailsInformation(firstUserElement, user1);
 
-const user2 = new Person("Abir", "Abir@gmail.com", 456); //! function constructor
 button.addEventListener("click", (e) => {
   e.preventDefault();
+  const user2 = new Person("Abir", "Abir@gmail.com", 456); //! function constructor
   showUserDetailsInformation(secondUserElement, user2);
 });
+
+const user3 = new Teacher("sajal", "sajal@gmail.com", 1234, "cse"); //! function constructor with inheritance example
+showUserDetailsInformation(thirdUserElement, user3);
